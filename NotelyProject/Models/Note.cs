4204b4fa3728ac4416e4c81849dd97f6705a5383
JSauceda10 <j.sauceda10@yahoo.com>
2020-03-21 00:59:09 -0700

    Added DB context class

---
+++ NotelyProject/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotelyProject.Models
+{
+    public class Note
+    {
+        public long id { get; set; }
+        public String subject { get; set; }
+        public String detail { get; set; }
+        public DateTime createdDate { get; set; }
+        public DateTime lastModified { get; set; }
+        public bool isDeleted { get; set; }
+
+    }
+}
--- NotelyProject/Startup.cs
+++ NotelyProject/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NotelyProject.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace NotelyProject
 {
@@ -25,6 +27,7 @@ namespace NotelyProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<NotelyDBContext>(options => options.UseSqlite(Configuration["Data:NotelyRestApi:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
